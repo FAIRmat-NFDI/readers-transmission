f06a34116392e3574c4383d47babfdd58751a981
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-09-11 16:29:05 +0200

    Test readers; add reference files for tests

--- tests/test_perkin_elmer_asc.py
+++ tests/test_perkin_elmer_asc.py
@@ -1,7 +1,25 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import pytest
 import pint
 
-from fairmat_readers_transmission.readers import (
+from fairmat_readers_transmission.perkin_elmers_asc import (
     read_attenuation_percentage,
     read_long_line,
     read_sample_name,
